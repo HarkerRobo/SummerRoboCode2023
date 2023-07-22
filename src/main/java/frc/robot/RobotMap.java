183e83046e82668ec2e7df0dcc43c80eeb06d286
26eddiez <26eddiez@students.harker.org>
2023-07-21 23:02:09 -0700

    added swervemodule documentation

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -35,31 +35,42 @@ public final class RobotMap {
     }
   
    public static final class SwerveModule {
+        //gear ratio 
         public static final double TRANSLATION_GEAR_RATIO = 1.0;
+        //diameter of the wheel
         public static final double WHEEL_DIAMETER = 1.0; 
 
+        //can chain 
+        public static final String CAN_CHAIN = "rio";
+        //id of translation motors 
         public static final int [] TRANSLATION_IDS = {0, 0, 0, 0};
 
+        //translation motors inverted
         public static final boolean [] TRANSLATION_INVERTS = {false, false, false, false};
 
+        //current limit constants for translation motors 
         public static final double TRANS_PEAK = 0.0;
         public static final double TRANS_CONTINUOUS  = 0.0;
         public static final double TRANS_PEAK_DUR = 0.0;
 
+        //ids for rotation motors
         public static final int [] ROTATION_IDS = {0,0,0,0};
 
+        //rotation motors inverted
         public static final boolean [] ROTATION_INVERTS = {false, false, false, false};
         
+       //current limit constants for rotation motors  
         public static final double ROT_PEAK = 0.0;
         public static final double ROT_CONTINUOUS = 0.0;
         public static final double ROT_PEAK_DUR = 0.0;
 
+        //offsets of cancoders of each swerve module 
         public static final double [] CAN_CODER_OFFSETS = {0.0,0.0,0.0,0.0};
 
+        //conversions from native units
         public static final double ROTATION_CONVERSION = Conversions.conversionConstant(Conversions.System.ANGLE, TRANSLATION_GEAR_RATIO, WHEEL_DIAMETER);
         public static final double VELOCITY_CONVERSION = Conversions.conversionConstant(Conversions.System.VELOCITY,TRANSLATION_GEAR_RATIO, WHEEL_DIAMETER);
         public static final double POSITION_CONVERSION = Conversions.conversionConstant(Conversions.System.POSITION,TRANSLATION_GEAR_RATIO, WHEEL_DIAMETER);
-
     }
 
     public static final class Claw {
--- src/main/java/frc/robot/util/SwerveModule.java
+++ src/main/java/frc/robot/util/SwerveModule.java
@@ -8,42 +8,49 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
 import frc.robot.RobotMap;
 import harkerrobolib.util.HSFalconBuilder;
 import harkerrobolib.wrappers.HSFalcon;
 
 public class SwerveModule {
+    //motors on the swerve modules
     private HSFalcon translation;
     private HSFalcon rotation; 
 
     private CANCoder canCoder;
 
+    //swerve module id
     private int ID;
 
+    //rotation kP
     private int kP = 0;
 
-    private int kS = 0;
+    private int kS = 0 ;
     private int kV = 0;
     private int kA = 0;
 
     public SwerveModule(int id){
         ID = id;
 
+        //configures translation and rotation motors
         translation = new HSFalconBuilder().invert(RobotMap.SwerveModule.TRANSLATION_INVERTS[id]).supplyLimit
             (RobotMap.SwerveModule.TRANS_PEAK, RobotMap.SwerveModule.TRANS_CONTINUOUS, RobotMap.SwerveModule.TRANS_PEAK_DUR).build
-            (RobotMap.SwerveModule.TRANSLATION_IDS[id], RobotMap.CAN_CHAIN);
+            (RobotMap.SwerveModule.TRANSLATION_IDS[id], RobotMap.SwerveModule.CAN_CHAIN);
 
         rotation = new HSFalconBuilder().invert(RobotMap.SwerveModule.ROTATION_INVERTS[id]).supplyLimit
             (RobotMap.SwerveModule.ROT_PEAK, RobotMap.SwerveModule.ROT_CONTINUOUS, RobotMap.SwerveModule.ROT_PEAK_DUR).build
-            (RobotMap.SwerveModule.ROTATION_IDS[id],RobotMap.CAN_CHAIN);
+            (RobotMap.SwerveModule.ROTATION_IDS[id],RobotMap.SwerveModule.CAN_CHAIN);
 
         init();
     }   
-
+    /**
+     * Sets cancoders to default
+     */
     private void init(){
         rotation.config_kP(0, kP);
-        translation.enableVoltageCompensation(false);
-        translation.configVelocityMeasurementWindow(32);
+        translation.enableVoltageCompensation(false); //disables voltage compensation 
+        translation.configVelocityMeasurementWindow(32); //number of samples measured 
         canCoder.configFactoryDefault();
         canCoder.clearStickyFaults();
         canCoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
@@ -52,11 +59,18 @@ public class SwerveModule {
         setAbsolutePosition();
         
     }
+    /**
+     * Sets translation and rotation motors to move to new state
+     * @param state new state 
+     */
     public void setAngleAndDrive(SwerveModuleState state){
         state = optimize(state);
         //translation.setVoltage(transLoop.getVoltage(state.speedMetersPerSecond, getSpeed()));
         rotation.set(ControlMode.Position,state.angle.getDegrees()/RobotMap.SwerveModule.ROTATION_CONVERSION);
     }
+    /*
+     * adjusts the angle of a swerve module state 
+     */
     public SwerveModuleState optimize (SwerveModuleState desiredState){
        double currentAngle = Math.toRadians(getAngle());
        double targetAngle = Math.IEEEremainder(desiredState.angle.getRadians(), Math.PI*2);
@@ -82,30 +96,44 @@ public class SwerveModule {
     return new SwerveModuleState(speed, Rotation2d.fromRadians(adjusted));
         
     }
+    /*
+     * resets the swerve module
+     */
     private void setAbsolutePosition(){
         double position = canCoder.getAbsolutePosition()-RobotMap.SwerveModule.CAN_CODER_OFFSETS[ID];
         rotation.setSelectedSensorPosition(position/RobotMap.SwerveModule.ROTATION_CONVERSION);
         zeroTranslation();
     }
-    public void zeroTranslation(){
+    private void zeroTranslation(){
         translation.setSelectedSensorPosition(0);
     }
+    /*
+     * returns the angle of the rotation motor 
+     */
     public double getAngle(){
         return rotation.getSelectedSensorPosition()*RobotMap.SwerveModule.ROTATION_CONVERSION;
     }
+    /*
+     * return speed of translation motor 
+     */
     public double getSpeed(){
         return translation.getSelectedSensorVelocity()*RobotMap.SwerveModule.VELOCITY_CONVERSION;
     }
+    /*
+     * returns position of translation motor
+     */
     public double getPosition(){
         return translation.getSelectedSensorPosition()*RobotMap.SwerveModule.POSITION_CONVERSION;
     }
+    //returns position and angle
     public SwerveModulePosition getCurrentPosition(){
         return new SwerveModulePosition(getPosition(),Rotation2d.fromDegrees(getAngle()));
     }
+    //returns speed and angle
     public SwerveModulePosition getCurrentState(){
         return new SwerveModulePosition(getSpeed(),Rotation2d.fromDegrees(getAngle()));
     }
-    //not done based on ids
+    //name of module on smart dashbaord 
     public static String moduleName(int id){
         String output = "motor id" + id;
         return output; 
